6cbb26fad1667e01d19641017ad367cc2e0cd94e
mystica2000 <mysticainf@gmail.com>
2022-03-01 16:19:55 +0530

    type alias

--- src/type-guard/type-interface.ts
+++ src/type-guard/type-interface.ts
@@ -5,4 +5,5 @@ const isCat:positive = true;
 console.log(isCat)
 console.log(typeof isCat) // boolean not positive since it is just a alias
 
+
 // type cannot be extended but can used with "|" or "&"
