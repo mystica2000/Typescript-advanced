79085b948a9761f5a999b15001a020c418be57ea
mystica2000 <mysticainf@gmail.com>
2022-03-01 16:02:08 +0530

    type alias

---
+++ type-interface.ts
@@ -0,0 +1,8 @@
+type positive = boolean;
+const isCat:positive = true;
+// alias 
+
+console.log(isCat)
+console.log(typeof isCat) // boolean not positive since it is just a alias
+
+// type cannot be extended but can used with "|" or "&"
