c41a176d07d7f7ef98b38519901013b9b0835d0b
mystica2000 <mysticainf@gmail.com>
2022-03-01 16:01:33 +0530

    guard using as and in operator

---
+++ guard.ts
@@ -0,0 +1,60 @@
+// Type Guards in function
+
+type SmartWatch = {
+    modelId:Number;
+    modelName:String;
+    brand:String;
+    features:String[];
+    price: Number;
+    waterProof: true | false;
+};
+
+
+type AnalogWatch = {
+    modelId:Number;
+    modelName:String;
+    brand:String;
+    price:Number;
+    waterProof: true | false;
+}
+
+
+const billingCounter = (watch : SmartWatch | AnalogWatch ) =>
+{
+    // using "in" operator
+    // if("features" in watch)
+    // {
+    //     console.log("Smart Watch")
+    // }
+    // else
+    // {
+    //     console.log("Analog Watch")
+    // }
+
+    // using "as" operator
+    if(isSmartWatch(watch))
+    {
+        console.log("Smart watch");
+    }
+    else
+    {
+        console.log("Analog Watch");
+    }
+}
+
+// using "as" - type assertion
+const isSmartWatch = (watch: SmartWatch | AnalogWatch) =>
+{
+    return (watch as SmartWatch).features != undefined; 
+}
+
+const watch:AnalogWatch = {
+    modelId:101,
+    modelName:"GTX",
+    brand:"Amazefit",
+    // features: ["Sports Mode","Sleep Tracking","GPS"],
+    price:1000,
+    waterProof:true
+}
+
+billingCounter(watch)
